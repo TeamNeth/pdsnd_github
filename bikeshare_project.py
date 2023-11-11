4aecc35eb15aee5e1504b3220fc5a1905eac456b
kenneth-ibekwe <dhackprove@gmail.com>
2023-11-11 09:27:51 +0100

    refactor: Optimize comment

--- bikeshare_project.py
+++ bikeshare_project.py
@@ -2,6 +2,9 @@ import time
 import pandas as pd
 import numpy as np
 
+# beginning of code
+
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
